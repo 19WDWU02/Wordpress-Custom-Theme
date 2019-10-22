40090472a20a60da904e003656d0e1ce5eda85dd
Richard Hpa <richard.hpa@bun2-206-05.local>
2019-10-23 11:57:24 +1300

    added in navigation items

--- functions.php
+++ functions.php
@@ -14,3 +14,21 @@ add_action('wp_enqueue_scripts', 'addCustomThemeFiles_1902');
 
 
 add_theme_support( 'post-thumbnails',  array( 'post' ) );
+
+add_image_size('icon', 50, 50, true);
+
+function addCustomMenus_1902(){
+    add_theme_support('menus');
+    // register_nav_menu('top_navigation', 'The Top Navigation is located at the top of each page.');
+    register_nav_menu( 'top_navigation', __( 'The Top Navigation is located at the top of each page.', '1902Custom' ) );
+}
+
+add_action('after_setup_theme', 'addCustomMenus_1902');
+
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
--- index.php
+++ index.php
@@ -7,25 +7,61 @@
 </head>
 <body>
 
+    <?php if(has_nav_menu( 'top_navigation' )): ?>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top_navigation" aria-controls="top_navigation" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#">Navbar</a>
+                <?php
+                wp_nav_menu( array(
+                    'theme_location'    => 'top_navigation',
+                    'depth'             => 2,
+                    'container'         => 'div',
+                    'container_class'   => 'collapse navbar-collapse',
+                    'container_id'      => 'top_navigation',
+                    'menu_class'        => 'nav navbar-nav',
+                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'            => new WP_Bootstrap_Navwalker(),
+                ) );
+                ?>
+            </div>
+        </nav>
+    <?php endif; ?>
+
     <div class="container">
         <?php if( have_posts() ): ?>
             <?php while( have_posts() ): the_post(); ?>
-                <div class="card mt-3">
+                <div class="card mb-2 mt-2">
                     <h5 class="card-header"><?php the_title(); ?></h5>
                     <div class="card-body">
-                        <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
-                        <div>
-                            <?php if( is_home() ): ?>
-                                <?php the_excerpt() ; ?>
-                            <?php else: ?>
-                                <?php the_content(); ?>
+                        <div class="row">
+                            <?php if(has_post_thumbnail()): ?>
+                                <?php if( is_home() ): ?>
+                                    <div class="col-12 col-md-3">
+                                        <?php the_post_thumbnail('medium', ['class' => 'img-fluid']); ?>
+                                    </div>
+                                <?php else: ?>
+                                    <div class="col-12 text-center mb-5">
+                                        <?php the_post_thumbnail('large', ['class' => 'img-fluid']); ?>
+                                    </div>
+                                <?php endif; ?>
                             <?php endif; ?>
+                            <div class="col">
+                                <div>
+                                    <?php if( is_home() ): ?>
+                                        <?php the_excerpt() ; ?>
+                                    <?php else: ?>
+                                        <?php the_content(); ?>
+                                    <?php endif; ?>
+                                </div>
+                                <?php if( !is_single() ): ?>
+                                    <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More</a>
+                                <?php endif; ?>
+                            </div>
                         </div>
-
-                        <?php if( !is_single() ): ?>
-                            <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More</a>
-                        <?php endif; ?>
-
                     </div>
                 </div>
             <?php endwhile; ?>
